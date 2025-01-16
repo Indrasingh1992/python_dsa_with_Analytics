b82c2f53b484191c2e0d3638f6b1a4180d018483
IndraSingh1992 <Indrabhushan.singh@hotmail.com>
2025-01-16 18:34:32 +0530

    Dsa with Analytics

---
+++ chromedriver_win32/selene.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+
+
+# driver = webdriver.Chrome("D:\web\chromedriver_win32\chromedriver-win64\chromedriver.exe")
+
+path = webdriver.ChromeService("D:\web\chromedriver_win32\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=path)
+
+
+driver.get('http://www.google.com/');
+
+time.sleep(5) # Let the user actually see something!
+
+search_box = driver.find_element_by_name('q')
+
+search_box.send_keys('ChromeDriver')
+
+search_box.submit()
+
+time.sleep(5) # Let the user actually see something!
+
+driver.quit()
---
+++ practice.py
@@ -0,0 +1,22 @@
+def insertion_sort(arr):
+    n = len(arr)
+    for current in range(1,n):
+        currentcard=arr[current]
+        correctposition=current-1
+
+        while correctposition>=0:
+            if(arr[correctposition]<currentcard):
+                break
+            else:
+                arr[correctposition+1]=arr[correctposition]
+                correctposition-=1
+            arr[correctposition+1] = currentcard
+    return arr
+
+
+
+
+
+unsorted_list = [12,25,11,34,90,22,10]
+sorted_list = insertion_sort(unsorted_list)
+print("Sorted Elements :", sorted_list)
