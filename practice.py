4f3412546658f34cb747e1e8d773001514a5e95c
IndraSingh1992 <Indrabhushan.singh@hotmail.com>
2025-03-22 09:59:17 +0530

    Pattern practice Questions

--- practice.py
+++ practice.py
@@ -1,22 +1,41 @@
-def insertion_sort(arr):
-    n = len(arr)
-    for current in range(1,n):
-        currentcard=arr[current]
-        correctposition=current-1
+def search_number(nums,target):
+    def binary_search_left(nums,target):
+        left,right=0,len(nums)-1
+        first_index=-1
 
-        while correctposition>=0:
-            if(arr[correctposition]<currentcard):
-                break
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]==target:
+                first_index=mid
+                right=mid-1
+
+            if nums[mid]<target:
+                left=mid+1
             else:
-                arr[correctposition+1]=arr[correctposition]
-                correctposition-=1
-            arr[correctposition+1] = currentcard
-    return arr
+                right=mid-1
+        return first_index
+    
+    def binary_search_right(nums,target):
+        left,right=0,len(nums)-1
+        last_index=-1
 
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]==target:
+                last_index=mid
+                left=mid+1
 
+            if nums[mid]<target:
+                left=mid+1
+            else:
+                right=mid-1
+        return last_index
+    
+    start = binary_search_left(nums, target)
+    end = binary_search_right(nums, target)
+ 
+    return [start, end]
 
-
-
-unsorted_list = [12,25,11,34,90,22,10]
-sorted_list = insertion_sort(unsorted_list)
-print("Sorted Elements :", sorted_list)
+nums=[5, 7, 7, 8, 8, 10]
+target=8
+print (search_number(nums,target))
