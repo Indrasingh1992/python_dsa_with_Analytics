176370019359bdbeda147d8a227d794721f80b41
IndraSingh1992 <Indrabhushan.singh@hotmail.com>
2025-03-31 06:32:44 +0530

    opps with custom Array list done

--- practice.py
+++ practice.py
@@ -1,15 +1,58 @@
-def plus_one(digits):
-    n = len(digits)
-    
-    for i in range(n - 1, -1, -1):
-        if digits[i] < 9:
-            digits[i] += 1
-            return digits
-        digits[i] = 0  # Set current digit to 0 and continue
-    
-    # If all digits were 9 (e.g., 999 → 1000), add a new leading 1
-    return [1] + digits
+import ctypes    #use it to create a array which will be having referential objects.
 
-# Example Test Cases
-print(plus_one([1, 2, 3]))    # Output: [1, 2, 4]
-print(plus_one([9, 9, 9]))    # Output: [1, 0, 0, 0]
+class CustomList:
+    def __init__(self):
+        initialCapacity=1
+        self.capacity = initialCapacity    #how much we can store in array
+        self.size = 0                   #how much we are using/stored in array
+        self.array= self.__create_array(self.capacity)                    # making sure here we don't have to create any list
+
+    def __create_array(self, capacity):   #creating a array of given capacity
+        return (capacity * ctypes.py_object)()   #this will create a refrential array of given capacity and type of py_object
+    def __resize(self, newCapacity):  #this will resize the array to new capacity
+        newArray = self.__create_array(newCapacity)
+        for i in range(self.size):  #this will copy the old array to new array
+            newArray[i] = self.array[i]
+        self.array = newArray #this will make the new array as the current array
+        self.capacity = newCapacity  #updating the capacity of the array
+
+    def append(self,item): #    this will add the item to the array
+        if self.size == self.capacity:   #if array is full then we will resize it
+            self.__resize(2 * self.capacity)   #doubling the capacity
+        self.array[self.size] = item   #storing the item in the array
+        self.size += 1   #incrementing the size of the array
+
+
+    
+
+    def __len__(self):   #this will return the size of the array
+        print("give me the logic to get the size of the array")
+        return self.size
+    
+    def __str__(self):   #this will return the string representation of the array
+        output = ""
+        for i in range(self.size):
+            output += str(self.array[i]) + ","
+        return "[" + output[:-1] + "]"  #removing the last comma from the string and returning it as string 
+    
+    def pop(self):
+        if self.size == 0:
+            return "List is empty, index error popped from empty list"
+        poppedItem = self.array[self.size - 1]  #getting the last item from the array
+        self.size -= 1  #decrementing the size of the array
+        return poppedItem  #returning the popped item
+    
+
+
+myList=CustomList()
+myList.append(1)   #adding 1 to the list
+myList.append(2)   #adding 2 to the list
+
+print(myList)   #printing the list
+print(myList.pop())   #printing the list   #printing the list
+print(myList)   #printing the list
+print(myList.pop())
+print(myList)   #printing the list
+print(myList.pop())   #printing the list
+print(len(myList))   #printing the size of the list
+        
